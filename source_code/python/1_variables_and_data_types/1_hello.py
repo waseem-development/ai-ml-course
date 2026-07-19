701b1329b370a4a5dc39a6cc812d18a208d73c4b
Hafiz Waseem Ahmed <hafizwaseemahmed2002@gmail.com>
2026-07-19 22:05:22 +0500

    print statement taught

---
+++ source_code/python/1_variables_and_data_types/1_hello.py
@@ -0,0 +1,37 @@
+print("Hello World 👋")
+print()
+
+
+print("I am Waseem and I love Python")
+print()
+
+
+print("I am learning Python and it is very fun and easy peezy")
+print()
+
+
+print("I hope you guys will enjoy it too")
+print()
+
+
+print(5)
+print()
+
+
+print(6)
+print()
+
+
+print(7)
+print()
+
+
+print(10+10)
+print(10-5)
+print(2*2) # MULTIPLICATION
+print(3/2) # DIVISION
+
+print()
+
+
+print("My age is ", 22)
