dd9c502428704cb192209158f03e53e9cf486fc8
waseemdevelopment <hafizwaseemahmed2002@gmail.com>
2026-08-18 21:54:37 +0500

    Made some changes in PPTX file

--- source_code/1_python/9_dictionaries/2_dictionary_elements_access.py
+++ source_code/1_python/9_dictionaries/2_dictionary_elements_access.py
@@ -1,7 +1,21 @@
-my_contact_dictionary = {
-    "ehsan":  124,
-    "musa": 4124,
-    "ahmed": 893,
-    "name": "Waseem",
-}
-print(my_contact_dictionary["ahmed"])
+# my_contact_dictionary = {
+#     "ehsan":  124,
+#     "musa": 4124,
+#     "ahmed": 893,
+#     "name": "Waseem",
+# }
+# print(my_contact_dictionary["ahmed"])
+
+def calculate_price(price, discount=0.1):
+    """
+    Calculate a discounted price.
+ 
+    price: the original price
+    discount: fraction to subtract (default 10%)
+    Returns: the final price after discount
+    """
+    return price - (price * discount)
+
+# print(calculate_price.__doc__)
+
+help(calculate_price)
