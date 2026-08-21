dc96c2a884375df147a7a6e27d9d7e7513e449be
waseemdevelopment <hafizwaseemahmed2002@gmail.com>
2026-08-21 21:17:45 +0500

    Upated the functions slides

---
+++ source_code/1_python/11_functions/5_mutable_default_argument_trap.py
@@ -0,0 +1,17 @@
+# BROKEN
+def add_item(item, cart=[]):
+    cart.append(item)
+    return cart
+ 
+print("********** add_item_safe **********\n",add_item("apple"))      # ['apple']
+print(add_item("banana"))   # ['apple', 'banana']  <- unexpected!
+
+# FIXED
+def add_item_safe(item, cart=None):
+    if cart is None:
+        cart = []
+    cart.append(item)
+    return cart
+
+print("\n\n********** add_item_safe **********\n",add_item_safe("apple"))   
+print(add_item_safe("banana")) 
