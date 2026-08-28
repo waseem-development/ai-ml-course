3bbaea261ac42008e4850852e56397c460cd28a0
waseem-development <hafizwaseemahmed2002@gmail.com>
2026-08-28 23:09:30 +0500

    Advance funcs done except recursion

--- source_code/1_python/12_advanced_functions/3_lambda_function.py
+++ source_code/1_python/12_advanced_functions/3_lambda_function.py
@@ -1,3 +1,7 @@
+# def multiply_by_2(n):
+#     return n * 2
+# print(multiply_by_2(3)) 
+
 x = lambda n: n * 2 # Lambda doesn't execute immediately
 # Python creates a function. It does not execute it.
 
@@ -5,10 +9,9 @@ x = lambda n: n * 2 # Lambda doesn't execute immediately
 
 # print(x(5))
 
-
 # ************ Lambda with Multiple Parameters ************
-add = lambda a, b: a + b
-# print(add(12,6))
+add = lambda a, b, c, d, e, f: a + b + c + d + e + f
+# print(add(12, 6, 1, 2, 3, 6))
 
 multiply = lambda a, b: a * b
 # print(multiply(5, 4))
@@ -16,19 +19,24 @@ multiply = lambda a, b: a * b
 # ************ Lambda with zero Parameters ************
 hello = lambda: "Hello Paxto!"
 # print(hello())
+# def greet():
+#     return "Hello Paxto!"
 
 # ************ Lambda Can Return Any Expression ************
 greet = lambda name: "Hello " + name
 # print(greet("Paxto"))
 
 is_even = lambda n: n % 2 == 0
-# print(is_even(10))
+# print(is_even(9))
+
+is_odd = lambda n: n % 2 != 0
+# print(is_odd(9))
 
 length = lambda text: len(text)
 # print(length("Python"))
 
 maximum = lambda a, b: max(a, b)
-# print(maximum(10, 20))
+print(maximum(10, 20))
 
 # ************ Lambda + max() / min() ************
 students = [
--- source_code/1_python/12_advanced_functions/4_closures.py
+++ source_code/1_python/12_advanced_functions/4_closures.py
@@ -1,14 +1,30 @@
+# def make_multiplier(factor):
+#     def multiply(n):
+#         return n * factor    # remembers "factor"
+#     return multiply
+ 
+# double = make_multiplier(2)
+# # triple = make_multiplier(3)
+ 
+# print(double(5))   # 10
+# print(double(10))
+# print(double(3))
+# # triple(5)   # 15
 
 
+# def make_counter():
+#     count = 0
 
-
-
-
-
-
-
-
-
+#     def increment():
+#         nonlocal count
+#         count += 1
+#         return count
+#     return increment
+ 
+# counter = make_counter()
+# print(counter())   # 1
+# print(counter())   # 2
+# print(counter())   # 3
 
 
 
@@ -32,7 +48,9 @@ counter = tasbeeh()
 while True:
     user_input = input("Press + to count: ")
 
-    if user_input == "+":
+    if user_input.strip() == "+":
         print(counter())
+    elif user_input.lower().strip() == "exit":
+        exit()
     else:
         print("Wrong Input")
