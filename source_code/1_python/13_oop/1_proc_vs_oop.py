8c6fc1c1e0eca14a6bf4498fafac8022fa0f4c97
waseemdevelopment <hafizwaseemahmed2002@gmail.com>
2026-09-06 20:45:06 +0500

    Intro to OOP (Theoretical Part) done

---
+++ source_code/1_python/13_oop/1_proc_vs_oop.py
@@ -0,0 +1,9 @@
+def dog_details():
+    dog_name = "Jack" # attribute
+
+    def bark(dog_name): # method
+        print(f"{dog_name} is a dog and dogs bark bow bow")
+
+    bark(dog_name)
+
+dog_details()
